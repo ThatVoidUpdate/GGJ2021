0f2e88c61695956815610ac9bd4f5c1392cca754
VoidUpdate <matthew.w.shaw74@gmail.com>
2021-01-29 20:05:47 +0000

    Added timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float StartTimeSeconds = 60;
+    public Text text;
+    public bool running = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running)
+        {
+            StartTimeSeconds -= Time.deltaTime;
+
+        }
+        text.text = $"Time Remaining: 00:{(int)StartTimeSeconds}"; 
+
+    }
+}
