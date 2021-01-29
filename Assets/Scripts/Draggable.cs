8259f30b31d0bcd07c72e7c5516e71118c7ae4e6
VoidUpdate <matthew.w.shaw74@gmail.com>
2021-01-29 19:25:57 +0000

    Added object dragging

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Draggable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Dragger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragger : MonoBehaviour
+{
+    public GameObject DraggingObject;
+
+    public float Distance;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.GetComponent<Draggable>())
+                {
+                    DraggingObject = hit.collider.gameObject;
+                    print("Grabbed Object: " + DraggingObject.name);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            DraggingObject = null;
+        }
+
+        if (DraggingObject != null)
+        {
+            DraggingObject.transform.position = transform.position + transform.forward * Distance;
+        }
+    }
+}
